ea929ec84c6af8f99a19c387b148cfa04bf43c9d
Heraldo Valenti <heraldovalenti@gmail.com>
2020-04-26 18:19:08 -0300

    Added gas loading and status interface
    Note also here that code doesn't compile and there are also test failures.

--- src/__tests__/Vehicle.test.ts
+++ src/__tests__/Vehicle.test.ts
@@ -7,3 +7,21 @@ describe('Vehicle should allow configuration of gas consumption rate', () => {
     expect(car.getConsumptionRate()).toBe(consumptionRate)
   })
 })
+
+describe('Vehicle should allow to load gas and inform the gas status', () => {
+  test('A new vehicle has no gas', () => {
+    const car = new Vehicle(1.5)
+    expect(car.getAvailableGas()).toBe(0)
+  })
+  test('Available gas should be the loaded amount', () => {
+    const car = new Vehicle(1.5)
+    car.loadGas(100)
+    expect(car.getAvailableGas()).toBe(100)
+  })
+  test('Loaded gas amount should be acumulative', () => {
+    const car = new Vehicle(1.5)
+    car.loadGas(100)
+    car.loadGas(20)
+    expect(car.getAvailableGas()).toBe(120)
+  })
+})
